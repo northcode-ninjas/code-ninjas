91793bde4ffeae850fc3f5135f3935b8a98fc95a
Joyfisher <joy.v.fisher@gmail.com>
2017-07-17 17:53:25 +0100

    started to add user models and seed data

---
+++ models/users.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    lowercase: true
+  },
+  avatar_url: {
+    type: String,
+    required: true,
+    lowercase: true
+  },
+  password: {
+      type: String,
+      required: true
+  },
+  progress: {
+      type: Number,
+      required: true
+  }
+});
+
+module.exports = mongoose.model('users', UserSchema);
---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const models = require('../models/users');
+
+router.get('/:username', function (req, res) {
+    const {username} = req.params;
+    return models.Users.find({username})
+    .then((welcomeUser) => {
+        if (welcomeUser.length < 1) {
+            return next({ status: 404, message: 'I do not recognise you young Ninja, reveal yourself to me again'})
+        }
+        else return res.status(200).json({welcomeUser, 'Welcome Back Ninja'})
+    })
+    .catch((err) => {
+        console.log(err)
+        if (err.name === 'CastError') {
+            return next({ status: 422, message: 'You have not started on the path to enlightenment, please sign up to begin' })
+        }
+    })
+})
+
+module.exports = router;
