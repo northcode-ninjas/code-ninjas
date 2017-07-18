076aff3bacf01907dd58d7f8611635d0fd5edefd
Janet <janetowen07@gmail.com>
2017-07-18 10:59:58 +0100

    Reorganise project structure

--- seed/data/user_data.js
+++ seed/data/user_data.js
@@ -1,3 +1,3 @@
 module.exports = [
     {username: 'loneNinja', email: 'loneninja@gmail.com', avatar_url: 'http://s3ninja.net/assets/images/ninja.jpg', password: 'iAmAnInJa', progress: 1 }
-]
+];
--- server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  test: {
-    DB: 'mongodb://localhost/questions-app-test'
-  },
-  dev: {
-    DB: 'mongodb://localhost/questions-app'
-  }
-};
