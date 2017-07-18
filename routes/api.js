4756db5dd4a09ca781347405c485c425ee74da9b
Janet <janetowen07@gmail.com>
2017-07-18 14:44:37 +0100

    route to get a single user

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,25 @@
 const router = require('express').Router();
+const models = require('../models/models');
+
+router.get('/', (req, res) => {
+    res.status(200).json('All good');
+});
+
+router.get('/users/:username', (req, res, next) => {
+    const {username} = req.params;
+    return models.Users.find({username})
+    .then((welcomeUser) => {
+        if (welcomeUser.length < 1) {
+            return next({ status: 404, message: 'I do not recognise you young Ninja, reveal yourself to me again'})
+        }
+        else return res.status(200).json({welcomeUser})
+    })
+    .catch((err) => {
+        console.log(err)
+        if (err.name === 'CastError') {
+            return next({ status: 422, message: 'You have not started on the path to enlightenment, please sign up to begin' })
+        }
+    })
+});
 
 module.exports = router;
