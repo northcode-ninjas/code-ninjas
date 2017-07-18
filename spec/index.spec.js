cb07747ba9a014a9517354e682855c831736dda5
janetowen07 <janetowen07@gmail.com>
2017-07-18 15:38:42 +0100

    Feat 17 (#8)
    * create api path in index file
    
    * created models file
    
    * route to get a single user
    
    * add email in user schema
    
    * test for api route passes
    
    * FIxed linting errors

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,2 +1,38 @@
 process.env.NODE_ENV = 'test';
+const { expect } = require('chai');
+const request = require('supertest');
+const server = require('../index');
+const mongoose = require('mongoose');
+const data = require('../seed/data/users');
+// const config = require('../config');
+const saveTestData = require('../seed/users.seed');
+// const db = config.DB[process.env.NODE_ENV] || process.env.DB;
 
+describe('API Routes', function () {
+  beforeEach(done => {
+    mongoose.connection.dropDatabase()
+    .then(() => saveTestData(data)
+      .then(() => done())
+      .catch(err => done(err))
+    );
+  });
+
+  after(done => {
+    mongoose.connection.close();
+    done();
+  });
+});
+  describe('GET /api/', function () {
+    it('responds with 200', function (done) {
+      request(server)
+        .get('/api/')
+        .expect(200)
+        .end((err, res) => {
+          if (err) done(err);
+          else {
+            expect(res.body).to.equal('All good');
+            done();
+          }
+        });
+    });
+  });
