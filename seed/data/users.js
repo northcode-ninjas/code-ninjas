3738ab5090db013e1b6fe9451df1432f3cc16fac
Joyfisher <joy.v.fisher@gmail.com>
2017-07-27 14:53:51 +0100

    [feat-98] avatar edited

--- seed/data/users.js
+++ seed/data/users.js
@@ -1,11 +1,10 @@
-const faker = require('faker');
 const {range} = require('underscore');
 
 const users = range(3).map((num) => {
     return {
         username: `loneninja${num}`,
         email: `loneninja${num}@gmail.com`,
-        avatar_url: faker.internet.avatar(),
+        avatar_url: 'http://img10.deviantart.net/b7ea/i/2015/104/d/f/lucy_ninja_by_waynemountan-d8isffj.png',
         level: 0
     };
 });
